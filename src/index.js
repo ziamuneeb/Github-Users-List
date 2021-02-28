798ab5fb667ccf456dd5f02b533d994f2cb901ba
ziamuneeb <ziamuneeb7@gmail.com>
2021-03-01 02:29:18 +0500

    update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDom from 'react-dom';
+import User from './user.js'
+import './index.css'
+import {useState , useEffect} from 'react';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+function App() {
+const url = 'https://api.github.com/users';
+
+const fetchData = async () => {
+  const request = await fetch(url);
+  const users = await request.json();
+  setUser(users);
+
+}
+const [user, setUser] = useState([]);
+console.log(user);
+
+useEffect(()=>{
+  fetchData();
+},[])
+
+return (
+  <User people = {user}></User>
 );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+ReactDom.render(<App></App>, document.getElementById('root'));
